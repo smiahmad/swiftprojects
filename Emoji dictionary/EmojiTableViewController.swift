7b3a5865d883605a17b34a7f61ba8ca60c0933cb
Imran Ahmad <smi_ahmad@hotmail.com>
2019-11-30 13:05:44 -0800

    Created a Tableview and the segue to new view controller works

---
+++ Emoji dictionary/EmojiTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  EmojiTableViewController.swift
+//  Emoji dictionary
+//
+//  Created by Syed Ahmad on 11/30/19.
+//  Copyright © 2019 Syed Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewController: UITableViewController {
+
+    // use Ctrl + Command + space bar to pring up the pop-up of emojis to enter them as below
+    var emojis = ["😀", "🚗", "⏰","🏖","🍏"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+
+        cell.textLabel?.text = emojis[indexPath.row]
+        
+
+        return cell
+    }
+ 
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        performSegue(withIdentifier: "ourSegue", sender: nil)
+    
+    }
+
+}
